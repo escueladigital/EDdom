a8c576501e5289699e571473ec97c8f13fa153fc
Camilo Rodríguez <aeroxmotion@hotmail.com>
2017-09-30 14:35:23 -0500

    refactor: se combinan las configuraciones de babel para cada ambiente

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,6 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   plugins: [
-    babel({
-      babelrc: false,
-      presets: [
-        ['env', { modules: false, loose: true }]
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties'
-      ]
-    })
+    babel()
   ]
 }
