7776af08643311cd97c1328d1c0946c9686be240
Camilo Rodríguez <aeroxmotion@hotmail.com>
2017-10-07 11:23:20 -0500

    test: pruebas correctas para la función query

--- test/query.spec.js
+++ test/query.spec.js
@@ -16,17 +16,25 @@ test('query()', t => {
 
   // Fastest
   const classElements = query('.title', wrapper)
-  const tagElements = query('span', wrapper)
-  const allElements = query('*', wrapper)
-  const idElement = query('#form-email')
+  const nativeClassElements = wrapper.getElementsByClassName('title')
 
-  t.true(classElements instanceof HTMLCollection && classElements.length === 1, 'should query with `getElementsByClassName`')
-  t.true(Array.isArray(idElement) && idElement.length === 1, 'should query with `getElementById`')
-  t.true(tagElements instanceof HTMLCollection && tagElements.length === 1, 'should query with `getElementsByTagName`')
-  t.true(allElements instanceof HTMLCollection && allElements.length === 4, 'should query with `getElementsByTagName`')
+  const tagElements = query('span', wrapper)
+  const nativeTagElements = wrapper.getElementsByTagName('span')
+
+  const allElements = query('*', wrapper)
+  const nativeAllElements = wrapper.getElementsByTagName('*')
+
+  const idElement = query('#form-email')
+  const nativeIdElement = document.getElementById('form-email')
 
   // Slowest
   const queryElements = query('input.email-input[type="email"]', wrapper)
+  const nativeQueryElements = wrapper.querySelectorAll('input.email-input[type="email"]')
 
-  t.true(queryElements instanceof NodeList && queryElements.length === 1, 'should query with `querySelectorAll`')
+  t.deepEqual(idElement, [nativeIdElement], 'should query with `getElementById`')
+  t.deepEqual(tagElements, nativeTagElements, 'should query with `getElementsByTagName`')
+  t.deepEqual(allElements, nativeAllElements, 'should query with `getElementsByTagName`')
+  t.deepEqual(classElements, nativeClassElements, 'should query with `getElementsByClassName`')
+
+  t.deepEqual(queryElements, nativeQueryElements, 'should query with `querySelectorAll`')
 })
